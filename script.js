9253d36b32cfd3b2bb63e3f9de067b634edbe8ee
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-11-10 19:46:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let counter = document.getElementById('counter');
+let btn = document.getElementById('incrementBtn');
+let count = 0;
+function increase() {
+	count++;
+	counter.innerText = count;
+}
+
